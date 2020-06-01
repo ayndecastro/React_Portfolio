aeb1c06ba02a6b808ab62a5af454acad25ea4409
ayndecastro <vincentayndecastro@gmail.com>
2020-05-31 21:11:00 -0500

    added a nodeJS server to deploy to heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('clent/build'));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
