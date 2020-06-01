6ba18851813dbee37d16508c52b4b2229411fe5d
ayndecastro <vincentayndecastro@gmail.com>
2020-05-31 21:12:52 -0500

    added a nodeJS server to deploy to heroku

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('clent/build'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
